3ee31f0997baea909d91987dc57dab44ab46de4a
sumariyanto <desoulmarco@gmail.com>
2021-11-25 07:54:08 +0700

    pagi update

--- access/urls.py
+++ access/urls.py
@@ -6,4 +6,5 @@ from .import views
 urlpatterns =[
    path('api/v1/access', views.AccessListApi.as_view(),name='access'),
    path('api/v1/access/<int:pk>', views.AccessDetailApi.as_view(),name='access_detail'),
+   path('api/v1/spost',views.AccessPostApi.as_view(),name="post-access"),
 ]
--- access/views/access_apiv1.py
+++ access/views/access_apiv1.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
 from datetime import *
-
+import json
 
 class AccessListApi(APIView):
    def get(self,request):
@@ -29,3 +29,30 @@ class AccessDetailApi(APIView):
         }
         return Response(data)
 
+class AccessPostApi(APIView):
+    def post(self,request,format=None):
+        # djson={"email":"sdfa@gmail.com","nama":"agung"}
+        data=json.loads(request.body.decode("utf-8")) 
+        # data =json.loads(request.data["formData"])
+        x=data["formData"]
+       
+        email=x['email'].lower()
+        nama=x['nama'].lower()
+        fullname=x['fullname'].lower()
+        phone=x['phone'].string()
+        data={
+            'email':email,
+            'nama':nama,
+            'fullname':fullname,
+            'phone':phone
+        }
+        print(x)
+        print(data)
+        return Response({'result':data,'json':x})
+    def get(self,request,format=None):
+        djson='{"email":"sdfa@gmail.com","nama":"agung"}'
+        data =json.loads(djson)
+        print(data)
+        return Response(data)
+        
+       
